916a8c4ea5591951ea6f117315e3112a18225c2b
ZF <zifoongl@gmail.com>
2022-03-23 00:01:06 +0800

    Lesson 7 - Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Button has been clicked", e);
+  };
+
+  const handle2Click = (name, e) => {
+    console.log("Button has been clicked by " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click Me!</button>
+      <button onClick={(e) => handle2Click("Pikachu!", e)}>Click2</button>
     </div>
   );
 };
