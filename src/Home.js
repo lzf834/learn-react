b349f288d0ddfa88ab7db759e41091aff59a14e8
ZF <zifoongl@gmail.com>
2022-03-24 16:19:02 +0800

    Lesson 8 - States

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
-const Home = () => {
-  const handleClick = (e) => {
-    console.log("Button has been clicked", e);
-  };
+import { useState } from "react";
 
-  const handle2Click = (name, e) => {
-    console.log("Button has been clicked by " + name, e.target);
+const Home = () => {
+  const [name, setName] = useState("zf");
+  const [age, setAge] = useState(23);
+
+  const handleClick = () => {
+    setName("John");
+    setAge(13);
   };
 
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <p>
+        {name} is {age} years old
+      </p>
       <button onClick={handleClick}>Click Me!</button>
-      <button onClick={(e) => handle2Click("Pikachu!", e)}>Click2</button>
     </div>
   );
 };
