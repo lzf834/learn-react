cea8e684ee114549c0669e82bb69855a6b4a913a
ZF <zifoongl@gmail.com>
2022-04-08 10:43:50 +0800

    Lesson 20 - Making a custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,13 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs") //extra s added
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Couldn't fetch data from source");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 500);
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
---
+++ src/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+//Custom hooks name must start with the word "use"
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Couldn't fetch data from source");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 500);
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
