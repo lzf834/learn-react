46ab2c72efba09477f36e1e3b6c97e0a2fa290dc
ZF <zifoongl@gmail.com>
2022-04-16 11:53:30 +0800

    Lesson 25 - Route Parameters

---
+++ src/BlogDetails.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+const BlogDetails = () => {
+  const { blog_id } = useParams();
+
+  return (
+    <div className="blog-details">
+      <h2>Blog Details - {blog_id}</h2>
+    </div>
+  );
+};
+
+export default BlogDetails;
