632fbe2566a8e27e978973d7dad2d28e98b7af53
ZF <zifoongl@gmail.com>
2022-04-21 11:20:13 +0800

    Lesson 29 - Making a POST Request

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,22 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Mario");
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("New Blog Added");
+      setIsPending(false);
+    });
   };
 
   return (
@@ -32,7 +44,8 @@ const Create = () => {
           <option value="Mario">Mario</option>
           <option value="Yoshi">Yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disable="true">Adding Blog...</button>}
       </form>
     </div>
   );
